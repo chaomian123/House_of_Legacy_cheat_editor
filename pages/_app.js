717aaf4a1e20fe257f0e15c8d379b5a51a6cbf0a
clare <892284133@qq.com>
2025-05-22 13:59:48 +0800

    fix: seo

--- pages/_app.js
+++ pages/_app.js
@@ -6,11 +6,10 @@ export default function App({ Component, pageProps }) {
   return (
     <>
       <Head>
-        <title>EasySave3 Editor</title>
-        <meta property='og:site_name' content='EasySave3 Editor' />
-        <meta property='og:image' content='https://es3.lol/logo.png' />
+        <title>house of legency cheat editor</title>
+        <meta property='og:site_name' content='house of legency cheat editor' />
         <meta name='keywords'
-          content='EasySave3, Save file editing, Save file manipulation, Save file management, Game save editor, Save file converter, EasySave3 compatibility, Save data modification, Online save file editor, Save file backup, Save file restore, Save file extraction, Save file compression, Save file encryption, Save file decryption, Cross-platform support, EasySave3 integration, Save file analysis, Save file troubleshooting, User-friendly EasySave3 interface, Auto-save detection, Save file validation, Save file synchronization, Save file sharing, Save file recovery, Save file manipulation tools, Save file versioning'
+          content='house of legency, house of legency cheat, house of legency cheat save file '
         />
 
         <meta name='viewport' content='initial-scale=1.0, width=device-width' />
