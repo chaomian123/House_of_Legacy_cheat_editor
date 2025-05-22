58298749c70fb9203eb06c98650a6a9a50a42a75
clare <892284133@qq.com>
2025-05-22 14:32:55 +0800

    fix: seo

--- pages/_app.js
+++ pages/_app.js
@@ -11,8 +11,11 @@ export default function App({ Component, pageProps }) {
         <meta name='keywords'
           content='house of legency, house of legency cheat, house of legency cheat save file '
         />
-
+        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1056917899569324"
+     crossorigin="anonymous"></script
         <meta name='viewport' content='initial-scale=1.0, width=device-width' />
+        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1056917899569324"
+     crossorigin="anonymous"></script>
       </Head>
 
       <ChakraProvider>
