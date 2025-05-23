99a0d41a49e5824179db31574b21556d1dc0ba93
clare <892284133@qq.com>
2025-05-23 16:10:32 +0800

    seo config

--- pages/_app.js
+++ pages/_app.js
@@ -9,11 +9,12 @@ export default function App({ Component, pageProps }) {
         <title>house of legency cheat editor</title>
         <meta property='og:site_name' content='吾今有世家修改器' />
         <meta name='keywords'
-          content='house of legency, house of legency cheat, house of legency cheat save file, 吾 今 有 世 家 修改器, 吾今有世家 修改器, 吾今有世家 '
+          content='house of legency, house of legency cheat, house of legency cheat save file, 吾 今 有 世 家 修改器, 吾今有世家 修改器, 吾今有世家'
         />
         <meta name="google-adsense-account" content="ca-pub-1056917899569324"></meta>
         <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1056917899569324"
      crossorigin="anonymous"></script>
+     <script src="https://analytics.ahrefs.com/analytics.js" data-key="KIhjCyEc3LpDR/QIvFuOKQ" async></script>
         <meta name='viewport' content='initial-scale=1.0, width=device-width' />
       </Head>
 
